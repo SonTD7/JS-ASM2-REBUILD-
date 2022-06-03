6109669248e883db22d74ac810e893c95ed6ba9b
FER201m-SP22 <sontddeveloper@gmail.com>
2022-06-04 05:17:29 +0700

    commit

--- js/index.js
+++ js/index.js
@@ -1,32 +1,93 @@
 // ======= creat-table=======
 let table = document.createElement("table");
-table.className = "table";
+table.className = "table mt-5";
 let tr = document.createElement("tr");
 table.appendChild(tr);
 tr.className = "tr";
 let thStt = document.createElement("th");
-thStt.innerText = "NO."
-thStt.className = "thStt col-1" 
 tr.appendChild(thStt);
+thStt.innerText = "NO.";
+thStt.className = "thStt col-1";
+
 let thName = document.createElement("th");
+tr.appendChild(thName);
 thName.innerText = "NAME";
 thName.className = "thName col-1";
-tr.appendChild(thName);
+
 let thAge = document.createElement("th");
-thAge.innerText="AGE";
-thAge.className="thAge col-1";
 tr.appendChild(thAge);
+thAge.innerText = "AGE";
+thAge.className = "thAge col-1";
+
 let thMathScores = document.createElement("th");
+tr.appendChild(thMathScores);
 thMathScores.innerText = "MATH SCORES";
 thMathScores.className = "thMathScores col-1";
-tr.appendChild(thMathScores);
-let thPhysicalScores=document.createElement("th");
-thPhysicalScores.innerText="PHYSICAL SCORES";
-thPhysicalScores.className="thPhysicalScores col-1";
-tr.appendChild(thPhysicalScores);
-let thChemicalScores=document.createElement("th");
-thChemicalScores.innerText="CHEMICAL SCORES";
-thChemicalScores.className="thChemicalScores col-1";
-tr.appendChild(thChemicalScores);
 
+let thPhysicalScores = document.createElement("th");
+tr.appendChild(thPhysicalScores);
+thPhysicalScores.innerText = "PHYSICAL SCORES";
+thPhysicalScores.className = "thPhysicalScores col-1";
+
+let thChemicalScores = document.createElement("th");
+tr.appendChild(thChemicalScores);
+thChemicalScores.innerText = "CHEMICAL SCORES";
+thChemicalScores.className = "thChemicalScores col-1";
+
+let thMediumScores = document.createElement("th");
+tr.appendChild(thMediumScores);
+thMediumScores.innerText = "MEDIUM SCORE";
+thMediumScores.className = "thMediumScore col-1";
+
+var stt = 0;
+
+
+//=======create input function=======
+document.getElementById("submit").onclick = function () {
+  var name = document.getElementById("id-name").value;
+  var age = document.getElementById("id-age").value;
+  var mathScores = document.getElementById("id-mathScores").value;
+  var physicalScores = document.getElementById("id-physicalScores").value;
+  var chemicalScores = document.getElementById("id-chemicalScore").value;
+
+  stt++;
+  content(name, age, mathScores, physicalScores, chemicalScores);
+};
+//=======create rows containing unlimited information=======
+var content = function (
+  stt,
+  name,
+  age,
+  mathScores,
+  physicalScores,
+  chemicalScores
+) {
+  let trCreatNew = document.createElement("tr");
+  table.appendChild(trCreatNew);
+  trCreatNew.id = "data" + stt;
+
+  let tdStt = document.createElement("td");
+  trCreatNew.appendChild(tdStt);
+  tdStt.innerText = stt;
+
+  let tdNameCreatNew = document.createElement("td");
+  trCreatNew.appendChild(tdNameCreatNew);
+  tdNameCreatNew.innerText = name;
+
+  let tdAgeCreatNew = document.createElement("td");
+  trCreatNew.appendChild(tdAgeCreatNew);
+  tdAgeCreatNew.innerText = age;
+
+  let tdMathScoresCreatNew = document.createElement("td");
+  trCreatNew.appendChild(tdMathScoresCreatNew);
+  tdMathScoresCreatNew.innerText = mathScores;
+
+  let tdPhysicalScoresCreatNew = document.createElement("td");
+  trCreatNew.appendChild(tdPhysicalScoresCreatNew);
+  tdPhysicalScoresCreatNew.innerText = physicalScores;
+
+  let tdChemicalScoresCreatNew = document.createElement("td");
+  trCreatNew.appendChild(tdChemicalScoresCreatNew);
+  tdChemicalScoresCreatNew.innerText = chemicalScores;
+};
 document.getElementById("root").appendChild(table);
